ee3ed270328f16a56debfc4d34f78164b8fa5382
Irm1256 <harusato620@gmail.com>
2026-02-12 11:45:18 +0000

    aaa

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const socket = io("https://your-game.onrender.com");
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
